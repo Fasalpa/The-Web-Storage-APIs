af053fc535402c85e0adfd0a7ed36243fd08f054
fasalpa <psfr97@gmail.com>
2026-04-13 10:40:44 -0500

    Agregar ul y estructura JS

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,13 +1,23 @@
-url = ""
 
-fetch(url) 
- .then(function(response) { 
- return response.json(); 
- }) 
- .then(function(data) { 
- // Datos del Pokémon aquí 
- console.log(data);
- }) 
- .catch(function(error) { 
- alert("¡Error! Pokémon no encontrado"); 
- });
+
+
+
+
+
+
+
+let historialGuardados = document.getElementById("historialGuardados");
+
+url = "https://pokeapi.co/api/v2/pokemon/";
+
+fetch(url)
+  .then(function (respuesta) {
+    return respuesta.json();
+  })
+  .then(function (datos) {
+    // Datos del Pokémon aquí
+    console.log(datos);
+  })
+  .catch(function (error) {
+    alert("¡Error! Pokémon no encontrado");
+  });

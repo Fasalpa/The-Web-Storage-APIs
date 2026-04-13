804e2f244fc872c1b561723367003bf3df151a80
fasalpa <psfr97@gmail.com>
2026-04-13 10:29:53 -0500

    Cambios estructura

---
+++ javaScript/app.js
@@ -0,0 +1,13 @@
+url = ""
+
+fetch(url) 
+ .then(function(response) { 
+ return response.json(); 
+ }) 
+ .then(function(data) { 
+ // Datos del Pokémon aquí 
+ console.log(data);
+ }) 
+ .catch(function(error) { 
+ alert("¡Error! Pokémon no encontrado"); 
+ });
